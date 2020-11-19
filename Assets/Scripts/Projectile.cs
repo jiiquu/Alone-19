01ea928ab4c95e18163302fd4efee570c1231987
jiiquu <jiiquu@gmail.com>
2020-11-19 16:29:19 +0200

    initializing repository

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float launchForce = 10.0f;
+    private Rigidbody rigidBody = null;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void Launch()
+    {
+        rigidBody.AddRelativeForce(Vector3.forward * launchForce, ForceMode.Impulse);
+        Destroy(gameObject, 5.0f);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class Weapon : MonoBehaviour
+{
+    public Transform barrel = null;
+    public GameObject projectilePrefab = null;
+    private XRGrabInteractable interactable = null;
+
+    private void Awake()
+    {
+        interactable = GetComponent<XRGrabInteractable>();
+    }
+
+    private void OnEnable()
+    {
+        interactable.onActivate.AddListener(Fire);
+    }
+
+    private void OnDisable()
+    {
+        interactable.onActivate.RemoveListener(Fire);
+    }
+
+    private void Fire(XRBaseInteractor interactor)
+    {
+        CreateProjectile();
+    }
+
+    public void CreateProjectile()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, barrel.position, barrel.rotation);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch();
+    }
+}
