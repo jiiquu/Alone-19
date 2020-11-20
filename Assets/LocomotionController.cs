ee6dde08b04392013b64e5b378382d3407c445b0
jiiquu <jiiquu@gmail.com>
2020-11-20 11:20:21 +0200

    add basic teleportation

---
+++ Assets/LocomotionController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class LocomotionController : MonoBehaviour
+{
+    public XRController teleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(teleportRay)
+        {
+            teleportRay.gameObject.SetActive(CheckIfActivated(teleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+}
