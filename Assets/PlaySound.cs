ca95564844b5f25ac5a96cdd83ce3bd586843c0a
jiiquu <jiiquu@gmail.com>
2020-12-01 09:40:43 +0200

    add visuals and voiceover

---
+++ Assets/PlaySound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float delay = 5;
+
+    void Start()
+    {
+        audioSource.PlayDelayed(delay);
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+}
